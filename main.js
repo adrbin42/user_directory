0b9c21ce47489b1a95251fad706d29ebc65e1259
adrienne <adriennebing@gmail.com>
2017-06-20 16:02:20 -0400

    styling profiles - not finished

--- main.js
+++ main.js
@@ -5,26 +5,17 @@ let data = require('./data.js');
 
 app.use(express.static('public'));
 
-app.get('/', function(req,res){
-  res.sendFile('index.html',{root: __dirname});
-});
+// app.get('/', function(req,res){
+//   res.sendFile('user_dir',{root: __dirname});
+// });
 
 app.engine('mustache', mustExp());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/getprofiles', function(req,res){
+app.get('/user_dir', function(req,res){
   res.render('user_dir',data);
 });
-// app.get('/getprofiles', function(req,res){
-//   res.render(data.users, {
-//     let userInfo;
-//     for(let i = 0;i<data.users.length;i++){
-//       userInfo +=
-//     }
-//   });
-// });
-
 
 // {"id":1,"username":"hjuza0","name":"Hamlen Juza","avatar"
 // :"https://robohash.org/blanditiisexercitationemquaerat.png?size=150x150&set=set1",
@@ -35,14 +26,7 @@ app.get('/getprofiles', function(req,res){
 // app.get('/news', function(req, res){
 //      res.json(o);
 // });
-// app.get('/news', function(req, res){
-//      res.json([{title: 'test', content: 'test desc'}, {title: 'test2', content: 'test2 desc'}])
-// }
 
-
-// app.get('/home_hello', function(req,res){
-//   res.sendFile('home_hello.html', {root: __dirname});
-// });
 app.listen(3000, function(req,res){
   console.log('Your app is running on localhost:3000');
 });
