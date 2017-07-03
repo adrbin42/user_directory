ddde173345d4d765d45142074cefb2a90de5d06d
adrienne <adriennebing@gmail.com>
2017-07-03 06:46:44 -0400

    make sure all changes are uploaded

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ let data = require('./data.js');
 
 app.use(express.static('public'));
 
-// app.get('/', function(req,res){
-//   res.sendFile('user_dir',{root: __dirname});
-// });
-
 app.engine('mustache', mustExp());
 app.set('views', './views');
 app.set('view engine', 'mustache');
@@ -17,15 +13,11 @@ app.get('/user_dir', function(req,res){
   res.render('user_dir',data);
 });
 
-// {"id":1,"username":"hjuza0","name":"Hamlen Juza","avatar"
-// :"https://robohash.org/blanditiisexercitationemquaerat.png?size=150x150&set=set1",
-// "email":"hjuza0@blogger.com","university":"Pomor State University","job":"Engineer
-// IV","company":"Stark, Feil and Bode","skills":["DMVPN","HDX","Rhino 3D"],"phone":"7-(397)813-7803",
-// "address":{"street_num":"78226","street_name":"Ryan","city":"Kamennogorsk","state_or_province":null,
-// "postal_code":"188950","country":"Russia"}}
-// app.get('/news', function(req, res){
-//      res.json(o);
-// });
+app.get('/user/:id', function(req,res){
+  console.log(req.params.id);
+  let i = req.params.id -1;
+  res.render('user',data.users[i]);
+});
 
 app.listen(3000, function(req,res){
   console.log('Your app is running on localhost:3000');
